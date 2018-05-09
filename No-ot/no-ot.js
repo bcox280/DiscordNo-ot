6e80c98401e6fa73c0a6d1a160a527a74deea909
bcox280 <blairacox@gmail.com>
2018-05-09 18:21:30 +1200

    Add basic example bot from https://gist.github.com/eslachance/3349734a98d30011bb202f47342601d3

---
+++ No-ot/no-ot.js
@@ -0,0 +1,40 @@
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+// Load the config.json file that contains our token and our prefix values.
+const config = require("../config.json");
+
+client.on("ready", () => {
+    // This event will run if the bot starts, and logs in, successfully.
+    console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+    client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+client.on("guildCreate", guild => {
+    console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+    client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+client.on("guildDelete", guild => {
+    console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+    client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+
+client.on("message", async message => {
+
+    if(message.author.bot) return;
+    if(message.content.indexOf(config.prefix) !== 0) return;
+
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    if(command === "ping") {
+        const m = await message.channel.send("Ping?");
+        m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+    }
+
+});
+
+client.login(config.token);
